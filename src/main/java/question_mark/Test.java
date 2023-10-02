77d92c3cbd5a92172d3d0671697c66955059dad3
Frank <seesawin@gmail.com>
2023-10-02 18:47:32 +0800

    修改01

--- src/main/java/question_mark/Test.java
+++ src/main/java/question_mark/Test.java
@@ -3,5 +3,6 @@ package question_mark;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 }
