5fa3589aa87db5dccfdbe2356c90ec85fe380b1e
FrankHsu <frank_hsu@simbo.com.tw>
2023-10-20 11:38:22 +0800

    add2

--- src/main/java/question_mark/Test.java
+++ src/main/java/question_mark/Test.java
@@ -6,5 +6,6 @@ public class Test {
         System.out.println("test rebase 02");
         System.out.println("test rebase 03");
         System.out.println("add");
+        System.out.println("add");
     }
 }
