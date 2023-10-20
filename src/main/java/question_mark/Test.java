a57355a316a1814e19b6a59ac09413d153301e48
FrankHsu <frank_hsu@simbo.com.tw>
2023-10-20 11:10:05 +0800

    feat: test rebase
    test01

--- src/main/java/question_mark/Test.java
+++ src/main/java/question_mark/Test.java
@@ -9,5 +9,9 @@ public class Test {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 }
