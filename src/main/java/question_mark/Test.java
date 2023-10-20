d5b0e0953d70c2eaa6b132b0fd9bbfd638e0cb4a
FrankHsu <frank_hsu@simbo.com.tw>
2023-10-20 11:22:31 +0800

    clear

--- src/main/java/question_mark/Test.java
+++ src/main/java/question_mark/Test.java
@@ -4,20 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("clear");
         System.out.println("test rebase 02");
-        System.out.println("test rebase 02");
-        System.out.println("test rebase 02");
-        System.out.println("test rebase 02");
-
         System.out.println("test rebase 03");
-        System.out.println("test rebase 03");
-        System.out.println("test rebase 03");
-        System.out.println("test rebase 03");
-        System.out.println("test rebase 03");
-
-        System.out.println("add");
-        System.out.println("add");
-        System.out.println("add");
-        System.out.println("add");
         System.out.println("add");
     }
 }
