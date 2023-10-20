e7e51251bca4bd2ae0732bd4aa4fc1d95b5ab064
FrankHsu <frank_hsu@simbo.com.tw>
2023-10-20 12:26:23 +0800

    dev put

--- src/main/java/question_mark/Test03.java
+++ src/main/java/question_mark/Test03.java
@@ -7,5 +7,6 @@ public class Test03 {
         System.out.println("insert at dev");
         System.out.println("patch at dev");
         System.out.println("put at dev");
+        System.out.println("del at dev");
     }
 }
