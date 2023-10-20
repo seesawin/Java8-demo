946e923af8c14ff01a6a2d2ba18bfa1dbe32e229
FrankHsu <frank_hsu@simbo.com.tw>
2023-10-20 11:14:20 +0800

    develop clear

--- src/main/java/question_mark/Test.java
+++ src/main/java/question_mark/Test.java
@@ -2,28 +2,10 @@ package question_mark;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        System.out.println("clear");
         System.out.println("test rebase 02");
         System.out.println("test rebase 02");
         System.out.println("test rebase 02");
         System.out.println("test rebase 02");
-
     }
 }
