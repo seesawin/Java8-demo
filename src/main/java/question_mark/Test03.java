97bb40f0f8d28ed80863aca1972b37088760f486
FrankHsu <frank_hsu@simbo.com.tw>
2023-10-20 12:08:11 +0800

    dev update

--- src/main/java/question_mark/Test03.java
+++ src/main/java/question_mark/Test03.java
@@ -3,5 +3,6 @@ package question_mark;
 public class Test03 {
     public static void main(String[] args) {
         System.out.println("init at dev");
+        System.out.println("update at dev");
     }
 }
